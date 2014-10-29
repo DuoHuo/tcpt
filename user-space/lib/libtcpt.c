e93a49ac8f732eb7de1d18e2431f85667583828a
Freeman Zhang <freeman.zhang1992@gmail.com>
2014-10-29 11:00:52 +0800

    add libtcpt framework

---
+++ user-space/lib/libtcpt.c
@@ -0,0 +1,33 @@
+/**
+ * Hash user's new password and store in file(the shadow)
+ */
+int reg_pwd(char *pwd)
+{
+
+}
+
+/**
+ * Compare password with the shadow, return positive if success
+ */
+int authenticate(char *pwd)
+{
+	return 0;
+}
+
+/**
+ * Mount secure path now and modify the init script so the path
+ * will be automatically mounted when power on
+ */
+int mount_list(char *pathlist)
+{
+
+}
+
+/**
+ * Make nod for keymanager device file
+ */
+int mknod_for_keymanager()
+{
+
+}
+
